034ddadb9c5792426c24ba878ef9d6815b9868d8
gifeh <gian.fehr@sbw-media.ch>
2024-03-25 16:31:31 +0100

    bittttteee

--- index.php
+++ index.php
@@ -13,12 +13,6 @@ try {
     die('Verbindung fehlgeschlagen: ' . $e->getMessage());
 }
 
-// Überprüfen, ob ein Benutzer angemeldet ist und Weiterleitung zum Spiel
-if (isset($_SESSION['userName'])) {
-    header('Location: game.php');
-    exit;
-}
-
 // Überprüfen, ob das Anmeldeformular gesendet wurde
 if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['userName'])) {
     // Überprüfen, ob der eingegebene Benutzername gültig ist
@@ -40,6 +34,9 @@ function getUserNames($db) {
     $userNames = $stmt->fetchAll(PDO::FETCH_COLUMN);
     return $userNames;
 }
+
+// Abmeldung des Benutzers, wenn er auf index.php zurückkommt
+unset($_SESSION['userName']);
 ?>
 
 <!DOCTYPE html>
@@ -52,18 +49,3 @@ function getUserNames($db) {
 </head>
 <body>
   <h1>Number Game</h1>
-  <?php if(isset($errorMessage)) echo "<p>$errorMessage</p>"; ?>
-  <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>">
-    <label for="userName">Select your username:</label>
-    <select name="userName" id="userName" style="width: 200px; height: 30px;">
-        <?php
-        $userNames = getUserNames($db);
-        foreach ($userNames as $userName) {
-            echo "<option value=\"$userName\">$userName</option>";
-        }
-        ?>
-    </select>
-    <button type="submit">Login</button>
-  </form>
-</body>
-</html>
